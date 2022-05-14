ff348fbd35ca3376f38dbbd2a9dad5d2f1a6b350
MARX1108 <boyuan@vt.edu>
2022-05-14 18:03:38 -0400

    import lib

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import Hero from "../src/components/hero";
+import CoursesSection from "../src/components/courses-section";
+// import courses from "../src/data/courses";
+import BlogpostsSection from "../src/components/blogposts-section";
+import { BlogPost } from "../src/components/types/blog-post";
+
+
+// type Props = {
+//   posts: BlogPost[];
+// };
+
+const courses = [
+  {
+    id: 1,
+    title: "test",
+    description: "description",
+    url: "https://dribbble.com"
+  }
+]
+
+const posts = [
+  {
+    title: "test",
+    description: "description",
+    date: "2022-4-1",
+    slug: "test",
+    readingTime: "20min"
+  }
+]
+
+const IndexPage = () => {
+  return (
+    <>
+      <Hero />
+      <CoursesSection courses={courses} />
+      <BlogpostsSection posts={posts} />
+    </>
+  );
+};
+
+export default IndexPage
