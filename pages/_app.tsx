bb5c63268924218598d7a952239b882d91f565dd
MARX1108 <boyuan@vt.edu>
2022-05-14 11:30:12 -0400

    header, footer setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/globals.css'
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/theme";
+import Layout from "../src/components/layout";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <Layout>
+        {/* <CmdPalette /> */}
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
---
+++ src/constants.tsx
@@ -0,0 +1,7 @@
+export const TWITTER_PROFILE = 'https://twitter.com/marxw1108';
+export const GITHUB_PROFILE = 'https://github.com/marx1108';
+export const YOUTUBE_CHANNEL =
+    'https://www.youtube.com/channel/UCTexaJMnN_Pv6TVueQ61-oQ';
+export const POLYWORK_PROFILE = 'https://polywork.com/nikolovlazar';
+export const TWITCH_CHANNEL = 'https://twitch.tv/nikolovlazar';
+export const PLAUSIBLE_LINK = 'https://plausible.io/nikolovlazar.com';
